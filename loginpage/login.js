70672f61567afc12fe3405ce9ee463e4b6f401fe
lrudolph333 <trey.ru.iii@gmail.com>
2024-02-15 12:37:57 -0800

    hardcoding email and pass in js

--- loginpage/login.js
+++ loginpage/login.js
@@ -5,8 +5,10 @@ const loginform = document.querySelector(".loginform");
 loginform.addEventListener("submit", function (e) {
   e.preventDefault();
   // getting data from localStorage
-  const storedEmail = localStorage.getItem("email");
-  const storedPassword = localStorage.getItem("password");
+  const storedEmail = "email@gmail.com";
+  const storedPassword = "pass";
+  // const storedEmail = localStorage.getItem("email");
+  // const storedPassword = localStorage.getItem("password");
   // Retrieve email and password from login form
   const email = document.querySelector(".loginform input[type=email]").value;
   const password = document.querySelector(
